6f6bf3d9e191ddd61209b79dfb6dda18893ff632
Evan Ye <applepie31415@gmail.com>
2013-07-13 10:08:53 -0700

    added JS click handling/highlighting and basic html form

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,49 @@
-$(document).ready(function (){
-  $('#divId').load('http://www.google.com');     
+var CLEARED_TAGS = ['A', 'P', 'FONT', 'LI', 'PRE', 'SPAN', 'TD'];
+var HIGHLIGHT_COLORS = ['#00FFFF', '#33CCCC', '#00FF00', '#FF3399'];
+var INDEX = 0;
+
+$(document).ready(function(){
+  $('.view').click(function(e){
+    e.preventDefault();
+    var element = $(e.target);
+    var type = element.get(0).tagName;
+
+    if(CLEARED_TAGS.indexOf(type) == -1)
+      return;
+
+    var classes = getCss(element);
+    appendForm(type, classes);
+    highlight(type, classes);
+    INDEX++;
+  });
 });
+
+function appendForm(type, classes){
+  $('<input>').attr({
+    id: 'param' + INDEX,
+    type: 'text'
+  }).appendTo('form');
+  $('<input>').attr({
+    id: 'value' + INDEX,
+    type: 'hidden',
+    value: type + '|' + classes.join('|')
+  }).appendTo('form');
+}
+
+function highlight(type, classes){
+  var class_str = classes.length > 0 ? '.' + classes.join(' .') + ' ' : '';
+  $(class_str + type).each(function(){
+    $(this).css('background', HIGHLIGHT_COLORS[INDEX]);
+  });
+}
+
+function getCss(element){
+  var classes = [];
+  var parent = element.parent();
+  while(parent[0] !== $('#scraper_pageView')[0]){
+    if(parent.attr('class'))
+      classes.push(parent.attr('class'));
+    parent = parent.parent();
+  }
+  return classes;
+}
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, abort
+from flask import render_template, request, abort, Markup
 from app import app
 import urllib2
 
@@ -6,10 +6,12 @@ import urllib2
 def index():
     return render_template('index.html')
 
-@app.route('/slide2.html')
+@app.route('/slide2.html', methods = ['GET', 'POST'])
 def slide2():
     url = request.args.get('url')
-    if url is None:
-        return abort(404, 'rar need url')
-    html = urllib2.urlopen(url).read()
-    return render_template('slide2.html', external_html = html)
+    if url:
+      html = Markup(urllib2.urlopen(url).read().decode('utf-8'))
+      return render_template('slide2.html', external_html = html)
+
+    form = request.form
+    print form
