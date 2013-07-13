105d6f5e726533830cc7a3a7082be3906afd8b14
Kiran Vodrahalli <knv@princeton.edu>
2013-07-13 04:46:00 -0700

    scraper working and tested with <li> and cat facts website

--- super_scraper.py
+++ super_scraper.py
@@ -3,15 +3,29 @@ import urllib2
 import re
 
 html = urllib2.urlopen("http://facts.randomhistory.com/interesting-facts-about-cats.html").read()
-soup = BeautifulSoup(html)
-texts = soup.find_all('li')
 
-for text in texts:
-	split_texts = re.split('<[a-z/]*>', text.encode('utf-8'))
-	for new_text in split_texts:
-		dict = re.findall(r'(?<!<)[a-zA-Z0-9./]*(?!<)', new_text)
-		list = [x for x in dict if not x is '']
-		print '======================='
-		if len(list) > 4:
-			print list
-		print '======================='
+def produceObjects(html, demarker):
+	ans = []
+	soup = BeautifulSoup(html)
+	texts = soup.find_all(demarker)
+	sentence = ""
+	for text in texts:
+		split_texts = re.split('<[a-z/]*>', text.encode('utf-8'))
+		for new_text in split_texts:
+			dict = re.findall(r'<?[a-zA-Z0-9./]*>?', new_text)
+			list = [x for x in dict if not x is '']
+			if (len(list) > 0):
+				if (list[0][:1] is not '<'):
+					string = ' '.join(str(item) for item in list)
+					if string[-1:] is '.':
+						sentence += string
+						if len(sentence) > 4:
+							ans.append(str(sentence))
+							sentence = ""
+					else:
+						if len(string) > 4:
+							sentence += string 
+	return ans
+
+objs = produceObjects(html, "li")
+				
